c8f39e34e81fce51bbc0607937a7659ee1b525ef
joaolucasalves010 <ylucasslalves@gmail.com>
2024-04-12 17:46:12 -0300

    Initial commit

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+// http://localhost:5173/ site localhost
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
