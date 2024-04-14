572107e5fd958e7e1085916c1e55b0ca6751ec98
joaolucasalves010 <ylucasslalves@gmail.com>
2024-04-14 09:51:35 -0300

    evento com função

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,11 +1,22 @@
 
 const Events = () => {
+  function handleClick(e) {
+    console.log('Testando função')
+    console.log(`A soma de 2 + 2 é ${2 + 2}`)
+    console.log(e.target)
+    console.log(e)
+  }
+
   return (
     <div>
       <div>
         <button onClick={() => {
-          console.log('Clicou no botão!')
+          console.log('Testando eventos!')
         }}>Clique aqui</button>
+      </div>
+      {/* 7 - Evento com função */}
+      <div>
+        <button onClick={handleClick}>Evento com função</button>
       </div>
     </div>
   )
