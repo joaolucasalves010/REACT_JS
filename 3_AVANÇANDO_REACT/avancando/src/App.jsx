59e505c8f18e798f48f7f9c2f262abd42f7ef7c1
joaolucasalves010 <ylucasslalves@gmail.com>
2024-04-16 14:17:50 -0300

    Importando imagens

--- 3_AVANÇANDO_REACT/avancando/src/App.jsx
+++ 3_AVANÇANDO_REACT/avancando/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 
+// 2 - Imagens em assets
+import night from './assets/night.jpg'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +13,8 @@ function App() {
         <h1>Avançando no react</h1>
         {/* 1 - Imagem em public */}
         <img src="/img.jpg" alt="Alguma imagem" />
+        {/* 2 - Imagem em assets */}
+        <img src={night} alt="Outra imagem" />
       </div>
     </>
   )
