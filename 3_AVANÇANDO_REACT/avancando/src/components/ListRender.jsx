1d5fbad87dbbfdedefeef61c6a65fe9dcf5750e4
joaolucasalves010 <ylucasslalves@gmail.com>
2024-04-17 14:43:40 -0300

    propriedade key

--- 3_AVANÇANDO_REACT/avancando/src/components/ListRender.jsx
+++ 3_AVANÇANDO_REACT/avancando/src/components/ListRender.jsx
@@ -5,11 +5,26 @@ const ListRender = () => {
 
   const [list] = useState(['João Lucas', 'Matheus Alves', 'Pedro', 'Kaio'])
 
+  const [users, setUsers] = useState([
+    {id: 1, name: "João Lucas", age: 15},
+    {id: 2, name: "Pedro", age: 17},
+    {id: 3, name: 'Matheus Alves', age: 16}
+  ])
+
   return (
     <div>
+      {/* 4 - render sem key */}
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      {/* 5 - render com key */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age} anos
+          </li>
         ))}
       </ul>
     </div>
