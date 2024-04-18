52c84cefe64e991f7479feb8a4524a78b31e6384
joaolucasalves010 <ylucasslalves@gmail.com>
2024-04-18 14:17:30 -0300

    if e else ternário

--- 3_AVANÇANDO_REACT/avancando/src/components/ConditionalRender.jsx
+++ 3_AVANÇANDO_REACT/avancando/src/components/ConditionalRender.jsx
@@ -4,12 +4,24 @@
 const ConditionalRender = () => {
 
   const x = true;
+  const name = 'João Lucas'
+
 
   return (
     <div>
       {/* 7 - conditional render */}
       <h3>Isso vai ser exibido?</h3>
       {x && <p>Sim!</p>}
+      {/* 8 - Else */}
+      {name == 'João Lucas' ? (
+        <div>
+          <p>Olá João Lucas!</p>
+        </div>
+      ) : (
+        <div>
+          <p>Ola {name}</p>
+        </div>
+      )}
     </div>
   )
 }
