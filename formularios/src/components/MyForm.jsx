a79cffa290601d32afdf1a0c7914fc2949efa4d0
joaolucasalves010 <ylucasslalves@gmail.com>
2024-05-03 13:55:09 -0300

    gerenciamento de dados com hook

--- formularios/src/components/MyForm.jsx
+++ formularios/src/components/MyForm.jsx
@@ -1,13 +1,26 @@
 import './MyForm.css'
 
+import { useState } from 'react'
+
 const myForm = () => {
+  // 3 - Gerenciamento de dados
+  const [name, setName] = useState()
+
+  const [email, setEmail] = useState()
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  console.log(name)
+
   return (
     <div>
       {/* 1 - Criação de form */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name='name' placeholder='Digite o seu nome:'/>
+          <input type="text" name='name' placeholder='Digite o seu nome:' onChange={handleName}/>
         </div>
         {/* 2 - Label envolvendo input */}
         <label>
