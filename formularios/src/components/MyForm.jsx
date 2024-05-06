cc43fb145f8805a7a47aa93cd58aa29af47064d1
joaolucasalves010 <ylucasslalves@gmail.com>
2024-05-06 17:39:28 -0300

    simplificando manipulação

--- formularios/src/components/MyForm.jsx
+++ formularios/src/components/MyForm.jsx
@@ -1,18 +1,18 @@
-import './MyForm.css'
+import "./MyForm.css";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 const myForm = () => {
   // 3 - Gerenciamento de dados
-  const [name, setName] = useState()
+  const [name, setName] = useState();
 
-  const [email, setEmail] = useState()
+  const [email, setEmail] = useState();
 
   const handleName = (e) => {
-    setName(e.target.value)
-  }
+    setName(e.target.value);
+  };
 
-  console.log(name)
+  console.log(name, email);
 
   return (
     <div>
@@ -20,17 +20,28 @@ const myForm = () => {
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name='name' placeholder='Digite o seu nome:' onChange={handleName}/>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome:"
+            onChange={handleName}
+          />
         </div>
         {/* 2 - Label envolvendo input */}
         <label>
           <span>E-mail:</span>
-          <input type="text" name='email' placeholder='Digite o seu e-mail' />
+          {/* 4 - Simplificando a manipulação */}
+          <input
+            type="text"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
-        <input type="submit" value="Enviar"/>
+        <input type="submit" value="Enviar" />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default myForm
+export default myForm;
